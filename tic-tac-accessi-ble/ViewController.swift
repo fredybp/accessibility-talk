05bb28e1f2ae0db9924ee91c045eb0642e4586f3
Frederico De Paola <fredericodepaola@gmail.com>
2019-02-16 16:28:25 -0200

    Initial Commit

---
+++ tic-tac-accessi-ble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tic-tac-accessi-ble
+//
+//  Created by Frederico Bechara De Paola on 16/02/19.
+//  Copyright © 2019 Frederico Bechara De Paola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ tic-tac-accessi-bleTests/tic_tac_accessi_bleTests.swift
@@ -0,0 +1,34 @@
+//
+//  tic_tac_accessi_bleTests.swift
+//  tic-tac-accessi-bleTests
+//
+//  Created by Frederico Bechara De Paola on 16/02/19.
+//  Copyright © 2019 Frederico Bechara De Paola. All rights reserved.
+//
+
+import XCTest
+@testable import tic_tac_accessi_ble
+
+class tic_tac_accessi_bleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
